f7892fd65645bb181838ee6ccfffdc1dccc70423
Kazimir84 <kav2007@ukr.net>
2021-07-09 18:40:53 +0300

    Create files with home work

---
+++ main.js
@@ -0,0 +1,20 @@
+console.log('------------- # 4')
+
+let btn = document.getElementById('btn');
+let nameInput = document.getElementById('name');
+let passwordInput = document.getElementById('password');
+let insertData = document.getElementById('insertData');
+
+btn.addEventListener('click', data);
+
+function data() {        
+  let dataName = nameInput.value;      
+  let dataPass = passwordInput.value;    
+    if (dataPass.length > 0 && dataName.length > 0) {
+        let allData = insertData.innerHTML = 'User name: ' + dataName + '</br>' + 'Password: ' + dataPass;
+        localStorage.setItem("User Name ", dataName);
+        localStorage.setItem("Password:", dataPass);
+        return allData;
+    } alert ('Введите имя и пароль');     
+};
+
